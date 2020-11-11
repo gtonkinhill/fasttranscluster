5b36e787b0e3c66d7aa1ac2ad544fafa852b94db
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2020-11-11 14:05:09 +1300

    speed and memory improvements for pileup input

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     url="https://github.com/gtonkinhill/fasttranscluster",
     install_requires=[
         'numpy', 'scipy', 'plotly', 'pyfastx', 'datetime', 'numba', 'Pyvcf',
-        'datatable'
+        'datatable', 'bitarray'
     ],
     python_requires='>=3.6.0',
     packages=['fasttranscluster'],
